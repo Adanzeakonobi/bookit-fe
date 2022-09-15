4cacdbd1ae196b70999b3b4b7a7101271b302869
JuanLP <juan.luis.palacios.p@gmail.com>
2022-09-15 16:00:32 -0500

    Add defaul user and values for store state

--- src/redux/vehicles/vehicles.js
+++ src/redux/vehicles/vehicles.js
@@ -1,4 +1,4 @@
-export default function reducer(state = [], action = {}) {
+export default function reducer(state = {visible:[], all:[], current:{id:1, price:1, name:'Perol', image:'https://via.placeholder.com/150'}, visible:true }, action = {}) {
   switch (action.type) {
     default:
       return state;
