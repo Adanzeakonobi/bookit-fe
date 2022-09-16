2c4c0e66e18f80f634180f5b0064a52283f59c84
JuanLP <juan.luis.palacios.p@gmail.com>
2022-09-16 16:24:17 -0500

    Add NavigationMenu jsx structure

--- src/components/NavigationPanel.js
+++ src/components/NavigationPanel.js
@@ -1,6 +1,36 @@
+import { Nav } from 'react-bootstrap';
+import { MdMenu } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+
 function NavigationPanel() {
   return (
-    <div className="NavigationPanel" />
+    <div className="NavigationPanel">
+      <div className="nav-container">
+        <MdMenu className="nav-menu-icon" />
+        <div className="nav-collalpse">
+          <Link className="nav-brand" to="/main">Book.it</Link>
+          <Nav>
+            <ul>
+              <li>
+                <Link to="/main">Vehicles</Link>
+              </li>
+              <li>
+                <Link to="/reservation">Reserve</Link>
+              </li>
+              <li>
+                <Link to="/user/reservations">My reservations</Link>
+              </li>
+              <li>
+                <Link to="/vehicles/new">Add vehicle</Link>
+              </li>
+              <li>
+                <Link to="/vehicles">Delete vehicle</Link>
+              </li>
+            </ul>
+          </Nav>
+        </div>
+      </div>
+    </div>
   );
 }
 
