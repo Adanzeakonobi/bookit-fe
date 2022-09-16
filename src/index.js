c7a1c8c99cc23c3c87596c7b4a88727b51c9be9a
JuanLP <juan.luis.palacios.p@gmail.com>
2022-09-16 08:43:41 -0500

    Set up navigation routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 import store from './redux/configureStore';
 import './index.css';
 import App from './App';
@@ -8,8 +9,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
 );
