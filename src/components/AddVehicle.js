9fa52241b360282611a0fcb705e1a83167eee8b5
JuanLP <juan.luis.palacios.p@gmail.com>
2022-09-19 06:08:44 -0500

    Add basic AddVehicle jsx structure

--- src/components/AddVehicle.js
+++ src/components/AddVehicle.js
@@ -1,6 +1,42 @@
+import {
+  Container, Row, Col, Alert, Form, Button,
+} from 'react-bootstrap';
+
 function AddVehicle() {
+  const errors = ['error 1', 'error 2'];
+  
   return (
-    <div className="AddVehicle" />
+    <Form>
+      <Container className="AddVehicle">
+        <Row>
+          <Col>
+            { errors.map((error) => (
+              <Alert key={error} variant="danger">
+                {error}
+              </Alert>
+            )) }
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={8}>
+            <div className="image-panel">
+              <input type="file" />
+            </div>
+          </Col>
+          <Col lg={4}>
+            <Row>
+              <Form.Control type="text" placeholder="Name" name="name" />
+            </Row>
+            <Row>
+              <Form.Control type="number" placeholder="Price" name="price" />
+            </Row>
+            <Row>
+              <Button variant="primary" type="submit">Create</Button>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </Form>
   );
 }
 
