bebaffd4f4f1ab92db600a9fe5a487bf90c01488
JuanLP <juan.luis.palacios.p@gmail.com>
2022-09-19 06:15:36 -0500

    Add preview loading for file input

--- src/components/AddVehicle.js
+++ src/components/AddVehicle.js
@@ -8,6 +8,31 @@ function AddVehicle() {
   const [{
     id, price, name, image,
   }, setVehicle] = useState({});
+
+  const onSelectFile = (e) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setVehicle({
+        id, name, price, image: undefined,
+      });
+      return;
+    }
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setVehicle({
+        id, name, price, image: reader.result,
+      });
+    };
+    reader.onerror = (error) => {
+      console.log(error);
+    };
+  };
+  const handleChange = (e) => {
+    setVehicle({
+      id, name, price, image, [e.target.name]: e.target.value,
+    });
+  };
   return (
     <Form>
       <Container className="AddVehicle">
@@ -23,7 +48,7 @@ function AddVehicle() {
         <Row>
           <Col lg={8}>
             <div className="image-panel">
-              <input type="file" />
+              <input type="file" onChange={onSelectFile} />
               {image ? <img src={image} alt={name} /> : 'Plese select an image' }
             </div>
           </Col>
