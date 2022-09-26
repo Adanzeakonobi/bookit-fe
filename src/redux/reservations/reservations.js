ab43c60f725a115364458307e236cafc20bf894d
JuanLP <juan.luis.palacios.p@gmail.com>
2022-09-26 17:33:27 -0500

    Connect get /reservations api

--- src/redux/reservations/reservations.js
+++ src/redux/reservations/reservations.js
@@ -1,10 +1,19 @@
 import client from '../../utils/client';
 
+const LOAD_SUCCESS = 'bookit/reservations/LOAD_SUCCESS';
+const LOAD_FALURE = 'bookit/reservations/LOAD_FALURE';
+
 const ADDRESERVATION_SUCCESS = 'bookit/reservations/ADDRESERVATION_SUCCESS';
 const ADDRESERVATION_FAILURE = 'bookit/reservations/ADDRESERVATION_FAILURE';
 
 export default function reducer(state = { reservations: [], error: undefined }, action = {}) {
   switch (action.type) {
+    case LOAD_SUCCESS: {
+      return { ...state, reservations: [...action.payload], error: undefined };
+    }
+    case LOAD_FALURE: {
+      return { ...state, reservations: [], error: action.payload };
+    }
     case ADDRESERVATION_SUCCESS: {
       const {
         city, date, vehicle_id: vehicleId,
@@ -27,6 +36,22 @@ export default function reducer(state = { reservations: [], error: undefined }, 
       return state;
   }
 }
+
+export const loadReservations = () => ((dispatch) => client
+  .get('/reservations').then(
+    (response) => {
+      dispatch({
+        type: LOAD_SUCCESS,
+        payload: response.data.reservations,
+      });
+    },
+    (error) => {
+      dispatch({
+        type: LOAD_FALURE,
+        payload: error.response?.data || error.messsage,
+      });
+    },
+  ));
 
 export const addReservation = (reservation) => ((dispatch) => client
   .post('/reservations', reservation).then(
