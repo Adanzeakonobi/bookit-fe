d47e54931eca8ae7c6f836c785c3298f44f50bba
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-09-27 18:44:02 +0100

    fix broken styles

--- src/layout/NoAuthLayout.js
+++ src/layout/NoAuthLayout.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import NavigationPanel from '../components/NavigationPanel';
 
 function NoAuthLayout({ children }) {
   return (
-    <div className="no-auth-layout">
-      <NavigationPanel />
+    <>
       {children}
-    </div>
+    </>
   );
 }
 
